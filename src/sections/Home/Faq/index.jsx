6942c32f880accf53b2c25dd5b48987bd9cf053a
ManpreetBhatti <manpreet@bhatti.net>
2021-03-09 20:06:29 -0500

    Imported AccordionItem -- has assignment to undeclared variable state error

--- src/sections/Home/Faq/index.jsx
+++ src/sections/Home/Faq/index.jsx
@@ -1,10 +1,20 @@
 import Section from '@htv/ui-kit/components/Section';
 import Text from '@htv/ui-kit/components/Text';
+import { Faqs } from './data';
+import AccordionItem from './AccordionItem';
 
 export default function Faq() {
   return (
     <Section backgroundColor='darkviolet'>
-      <Text type='heading2'>Faq</Text>
+      <Text type='heading2' color='#312F3F'>
+        console.log(“
+        <span style={{ color: 'white' }}>Frequently Asked Questions</span>”)
+        <div>
+          {Faqs.map((faq, i) => {
+            return <AccordionItem {...faq} />;
+          })}
+        </div>
+      </Text>
     </Section>
   );
 }
