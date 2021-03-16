1bb9a614b712bcfa00ed92f96af073efbaa4d232
ManpreetBhatti <manpreet@bhatti.net>
2021-03-16 03:39:13 -0400

    Organized elements and positioned title

--- src/sections/Home/Faq/index.jsx
+++ src/sections/Home/Faq/index.jsx
@@ -9,6 +9,7 @@ import {
   transparentQMStyle,
   filledQMStyle,
   wrapSection,
+  titleStyle,
 } from './Faq.module.scss';
 
 export default function Faq() {
@@ -19,21 +20,21 @@ export default function Faq() {
         alt='Transparent question marks'
         className={transparentQMStyle}
       />
-      <Text type='heading2' color='darkviolet'>
+      <Text type='body1' color='darkviolet' className={titleStyle}>
         console.log(“
-        <Text type='heading2' as='span'>
+        <Text type='body1' as='span'>
           Frequently Asked Questions
         </Text>
         ”)
-        <div className={wrapAccordion}>
-          <AccordionItem title={Faqs[0].title} content={Faqs[0].content} />
-          <AccordionItem title={Faqs[1].title} content={Faqs[1].content} />
-          <AccordionItem title={Faqs[2].title} content={Faqs[2].content} />
-          <AccordionItem title={Faqs[3].title} content={Faqs[3].content} />
-          <AccordionItem title={Faqs[4].title} content={Faqs[4].content} />
-          <AccordionItem title={Faqs[5].title} content={Faqs[5].content} />
-        </div>
       </Text>
+      <div className={wrapAccordion}>
+        <AccordionItem title={Faqs[0].title} content={Faqs[0].content} />
+        <AccordionItem title={Faqs[1].title} content={Faqs[1].content} />
+        <AccordionItem title={Faqs[2].title} content={Faqs[2].content} />
+        <AccordionItem title={Faqs[3].title} content={Faqs[3].content} />
+        <AccordionItem title={Faqs[4].title} content={Faqs[4].content} />
+        <AccordionItem title={Faqs[5].title} content={Faqs[5].content} />
+      </div>
       <img
         src={filledQM}
         alt='Filled question marks'
