a08594f47e6ba007282ea64e8aa2992e6dda7193
ManpreetBhatti <manpreet@bhatti.net>
2021-03-22 03:43:30 -0400

    Fixed rotation of accordion icon

--- src/sections/Home/Faq/AccordionItem.jsx
+++ src/sections/Home/Faq/AccordionItem.jsx
@@ -9,23 +9,20 @@ import {
   wrapContent,
   wrappingDiv,
   wrappingText,
+  hiddenDiv,
 } from './AccordionItem.module.scss';
 
 export default function AccordionItem(props) {
   const [expand, setExpand] = useState(false);
   return (
     <div className={wrappingDiv}>
-      <Card
-        type='flat'
-        backgroundColor='darkviolet'
-        className={wrapCard}
-        onClick={() => setExpand((expand) => !expand)}
-      >
+      <Card type='flat' backgroundColor='darkviolet' className={wrapCard}>
         <Text
           type='body1'
           transform='uppercase'
           weight='normal'
           className={wrappingText}
+          onClick={() => setExpand(!expand)}
         >
           <Text
             type='body1'
@@ -36,7 +33,9 @@ export default function AccordionItem(props) {
           </Text>
           &nbsp;{props.title}
         </Text>
-        {expand && <div className={wrapContent}>{props.content}</div>}
+        <div className={`${!expand ? hiddenDiv : wrapContent}`}>
+          {props.content}
+        </div>
       </Card>
     </div>
   );
