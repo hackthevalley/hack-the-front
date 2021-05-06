653250a6b4985d4cdf79253c8ddfbe5c87cab67f
Pravinthan Prabagaran <pravinthan.prabagaran@gmail.com>
2021-05-06 00:21:18 -0400

    Added props for supported file extensions and file input name attribute; Improved error logic so that it will display to the user that they dropped in an invalid file; Added comments to explain the props

--- src/components/FileDropzone/index.jsx
+++ src/components/FileDropzone/index.jsx
@@ -14,9 +14,20 @@ import {
   text,
 } from './FileDropzone.module.scss';
 
-export default function FileDropzone({ onUpload, hasError = false }) {
+export default function FileDropzone({
+  // Callback function that is called when a file is chosen
+  onUpload,
+  // List of supported file extensions
+  supportedFileExtensions,
+  // File input element's name attribute
+  fileInputNameAttribute,
+  // Can be set to true to visibly show the user that there is an error (e.g. in the form)
+  hasError = false,
+}) {
   const [isFileHovering, setFileHovering] = useState(false);
   const [file, setFile] = useState(null);
+  // Used for when the user drops in a file that is invalid, in which case we show the user
+  const [isWrongFileExtension, setIsWrongFileExtension] = useState(false);
   const fileInputRef = createRef();
 
   const containsFiles = (e) => {
@@ -43,17 +54,27 @@ export default function FileDropzone({ onUpload, hasError = false }) {
 
   const onDrop = (e) => {
     e.preventDefault();
-    if (e.dataTransfer.files.length) {
-      if (e.dataTransfer.files[0].name)
-        fileInputRef.current.files = e.dataTransfer.files;
-      setFile(() => e.dataTransfer.files[0]);
-      onUpload(e.dataTransfer.files[0]);
-    }
     setFileHovering(() => false);
+    if (e.dataTransfer.files.length && e.dataTransfer.files[0].name) {
+      // Check if the file extension is supported
+      if (
+        supportedFileExtensions.includes(
+          e.dataTransfer.files[0].name.split('.').pop(),
+        )
+      ) {
+        fileInputRef.current.files = e.dataTransfer.files;
+        setIsWrongFileExtension(() => false);
+        setFile(() => e.dataTransfer.files[0]);
+        onUpload(e.dataTransfer.files[0]);
+      } else {
+        setIsWrongFileExtension(() => true);
+      }
+    }
   };
 
   const onFileChange = (e) => {
     if (e.target.files.length) {
+      setIsWrongFileExtension(() => false);
       setFile(() => e.target.files[0]);
       onUpload(e.target.files[0]);
     }
@@ -68,7 +89,7 @@ export default function FileDropzone({ onUpload, hasError = false }) {
       className={classNames(
         dropzone,
         isFileHovering && dropzoneHover,
-        hasError && dropzoneError,
+        (hasError || isWrongFileExtension) && dropzoneError,
       )}
       onDragOver={onDragover}
       onDragLeave={onDragleave}
@@ -97,7 +118,10 @@ export default function FileDropzone({ onUpload, hasError = false }) {
           </Text>
         </div>
         <Text color='gray' type='meta1'>
-          Supports: PDF
+          Supports:{' '}
+          {supportedFileExtensions
+            .map((fileExtension) => fileExtension.toUpperCase())
+            .join(', ')}
         </Text>
       </div>
 
@@ -105,8 +129,10 @@ export default function FileDropzone({ onUpload, hasError = false }) {
         ref={fileInputRef}
         type='file'
         required
-        name='resume'
-        accept='.pdf'
+        name={fileInputNameAttribute}
+        accept={supportedFileExtensions
+          .map((fileExtension) => `.${fileExtension}`)
+          .join()}
         className={fileInput}
         onChange={onFileChange}
       />
@@ -116,5 +142,18 @@ export default function FileDropzone({ onUpload, hasError = false }) {
 
 FileDropzone.propTypes = {
   onUpload: PropTypes.func.isRequired,
+  supportedFileExtensions: PropTypes.arrayOf(
+    (propValue, key, componentName, location, propFullName) => {
+      if (
+        propValue.filter((fileExtension) => fileExtension.includes('.'))
+          .length > 0
+      ) {
+        return new Error(
+          `Invalid prop '${propFullName}' supplied to '${componentName}'. '${propFullName}' is an array of file extension strings (e.g. ["pdf", "docx"])`,
+        );
+      }
+    },
+  ),
+  fileInputNameAttribute: PropTypes.string,
   hasError: PropTypes.bool,
 };
