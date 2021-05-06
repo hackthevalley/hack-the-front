2098e9faae09c70782d7fa3dbe8ef22691ed6ac0
ManpreetBhatti <manpreet@bhatti.net>
2021-05-05 23:52:51 -0400

    Update

--- src/components/Checkbox/index.jsx
+++ src/components/Checkbox/index.jsx
@@ -1,17 +1,17 @@
 import { checkbox, text } from './Checkbox.module.scss';
 
-export default function Checkbox(label) {
+export default function Checkbox(props) {
     return (
         <div>
-            <label className={text} htmlFor='agreement_text'>
-                {label.text}
-            </label>
             <input
                 className={checkbox}
                 name='agreement_text'
                 type="checkbox"
-                value={label.text}
+                value={props.text}
             />
+            <label className={text} htmlFor='agreement_text'>
+                {props.text}
+            </label>
         </div>
     );
 }
