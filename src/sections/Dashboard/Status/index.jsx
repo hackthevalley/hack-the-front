648db639ea871d5693580bedcde332a5c4f3576d
AmandaTang849 <amandatang88888@gmail.com>
2021-05-11 23:40:56 -0400

    application status implementation

--- src/sections/Dashboard/Status/index.jsx
+++ src/sections/Dashboard/Status/index.jsx
@@ -2,11 +2,53 @@ import Text from '@htv/ui-kit/components/Text'
 import Card from '@htv/ui-kit/components/Card'
 import Button from '@htv/ui-kit/components/Button'
 import {
-    statusText,
     submittedText,
+    acceptedText,
+    declinedText,
+    statusText,
     applicationButton,
     appDueText
 } from './Status.module.scss';
+
+function CurrentStatus(prop) {
+    if (prop.status == "submitted") {
+        return (
+            <Text className={submittedText} lineHeight='relaxed' align='start' type='heading2'>
+                Submitted!
+                <Button className={applicationButton}>
+                    View Application
+                </Button>
+            </Text>
+        );
+    } else if (prop.status == "notSubmitted") {
+        return (
+            <Text className={submittedText} lineHeight='relaxed' align='start' type='heading2'>
+                Not Submitted.
+                <Button className={applicationButton}>
+                    Start Application
+                </Button>
+            </Text>
+        );
+    } else if (prop.status == "accepted") {
+        return (
+            <Text className={acceptedText} lineHeight='relaxed' align='start' type='heading2'>
+                ACCEPTED!
+                <Button className={applicationButton}>
+                    View Application
+                </Button>
+            </Text>
+        );
+    } else if (prop.status == "declined") {
+        return (
+            <Text className={declinedText} lineHeight='relaxed' align='start' type='heading2'>
+                Declined.
+                <Button className={applicationButton}>
+                    View Application
+                </Button>
+            </Text>
+        );
+    }
+}
 
 export default function Status() {
     return (
@@ -14,12 +56,7 @@ export default function Status() {
             <Text className={statusText} lineHeight='relaxed' align='start' type='body1'>
                 Current Application Status
             </Text>
-            <Text className={submittedText} lineHeight='relaxed' align='start' type='heading2'>
-                Submitted!
-                <Button className={applicationButton}>
-                    View Application
-                </Button>
-            </Text>
+            <CurrentStatus status="submitted" />
             <Text className={appDueText} lineHeight='relaxed' align='start' type='body2'>
                 Application due on 
                 <Text lineHeight='relaxed' align='start' type='body2'>
