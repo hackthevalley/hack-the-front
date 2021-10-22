bc23d7c43d97eec3a9f029874ea2714275a4595d
Manpreet Bhatti <manpreet@bhatti.net>
2021-10-22 00:19:34 -0400

    Updated HTV date in FAQs (#43)
    * Update HTV date
    
    * Update package.json
    
    * Update dates based on gatsby-config module

--- src/sections/Home/Faq/data.js
+++ src/sections/Home/Faq/data.js
@@ -1,3 +1,5 @@
+import { graphql, StaticQuery } from 'gatsby';
+
 import Text from '@htv/ui-kit/components/Text';
 
 export const Faqs = [
@@ -86,7 +88,25 @@ export const Faqs = [
       </>
     ),
     content: (
-      <p>Hack The Valley will be taking place on October 15-17, 2021.</p>
+      <StaticQuery
+        query={graphql`
+          {
+            site {
+              siteMetadata {
+                startDate(formatString: "MMMM D")
+                endDate(formatString: "D, YYYY")
+              }
+            }
+          }
+        `}
+        render={(data) => (
+          <p>
+            Hack The Valley will be taking place on{' '}
+            {data.site.siteMetadata.startDate} -{' '}
+            {data.site.siteMetadata.endDate}.
+          </p>
+        )}
+      />
     ),
   },
   {
