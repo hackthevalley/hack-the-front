9c15849e8c8bc854506c2387169b08ce134166b8
fpunny <frederic.pun@gmail.com>
2021-11-08 11:49:35 -0500

    owo sponsors

--- src/sections/Home/Sponsors/index.jsx
+++ src/sections/Home/Sponsors/index.jsx
@@ -12,6 +12,7 @@ import * as styles from './Sponsors.module.scss';
 export default function Sponsors() {
   const { sponsors, site } = useStaticQuery(query);
   const sponsorsEnabled = site.siteMetadata.featureFlags.sponsors;
+  console.log(sponsors);
 
   return (
     <Section
@@ -49,38 +50,41 @@ export default function Sponsors() {
       </Text>
       {sponsorsEnabled && (
         <Card className={styles.sponsors} backgroundColor='white' type='flat'>
-          {sponsors.nodes.map((tier) => (
-            <div key={tier.recordId} style={{ '--scale': tier.data.Scale }}>
-              <Text transform='uppercase' type='meta1' as='h3'>
-                {tier.data.Name} Sponsors
-              </Text>
-              <ul className={styles.sponsors__list}>
-                {tier.data.Sponsors.map((sponsor) => (
-                  <li key={sponsor.recordId}>
-                    <a
-                      className={styles.sponsor__link}
-                      href={sponsor.data.Link}
-                      target='_blank'
-                      rel='noopenner noreferrer'
-                    >
-                      <GatsbyImage
-                        image={getImage(sponsor.data.Logo.localFiles[0])}
-                        style={
-                          sponsor.data.Scale !== null
-                            ? { '--scale': tier.data.Scale * sponsor.data.Scale }
-                            : undefined
-                        }
-                        alt={`${sponsor.data.Name}'s logo`}
-                        className={styles.sponsor__asset}
-                        objectPosition='center'
-                        objectFit='contain'
-                      />
-                    </a>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          ))}
+          {sponsors.nodes
+            .filter(tier => (tier.data.Sponsors ?? []).length)
+            .map((tier) => (
+              <div key={tier.recordId} style={{ '--scale': tier.data.Scale }}>
+                <Text transform='uppercase' type='meta1' as='h3'>
+                  {tier.data.Name} Sponsors
+                </Text>
+                <ul className={styles.sponsors__list}>
+                  {tier.data.Sponsors.map((sponsor) => (
+                    <li key={sponsor.recordId}>
+                      <a
+                        className={styles.sponsor__link}
+                        href={sponsor.data.Link}
+                        target='_blank'
+                        rel='noopenner noreferrer'
+                      >
+                        <GatsbyImage
+                          image={getImage(sponsor.data.Logo.localFiles[0])}
+                          style={
+                            sponsor.data.Scale !== null
+                              ? { '--scale': tier.data.Scale * sponsor.data.Scale }
+                              : undefined
+                          }
+                          alt={`${sponsor.data.Name}'s logo`}
+                          className={styles.sponsor__asset}
+                          objectPosition='center'
+                          objectFit='contain'
+                        />
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            ))
+          }
         </Card>
       )}
     </Section>
