86d4c677e50b2f07c53ca3a383285ea6405cf6e4
fpunny <frederic.pun@gmail.com>
2021-12-10 01:37:30 -0500

    task: Fixed sponsor image resizing, added description to inputs

--- src/sections/Application/Personal/index.jsx
+++ src/sections/Application/Personal/index.jsx
@@ -44,7 +44,7 @@ export default function Personal({ userInfo }) {
         <FormField
           index='2'
           fieldProps={{
-            description: 'For institutional emails, please check your spam for the confirmation email',
+            description: 'For any institutional email, do check your spam for the confirmation',
             disabled: true,
             readOnly: true,
           }}
