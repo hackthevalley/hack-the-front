8887df4cf3fef2450ae3bd1e09df5a41b147216a
Benjamin Saobuppha <65824672+BenjaminSao@users.noreply.github.com>
2022-09-20 20:36:54 -0400

    Add sponsorship (#76)
    * re-enabled sponsors, added custom rendering for 'Support' type sponsors
    
    * refactoring

--- src/sections/Home/Sponsors/index.jsx
+++ src/sections/Home/Sponsors/index.jsx
@@ -10,8 +10,8 @@ import { ReactComponent as SponsorBackground } from '../../../images/sponsor.svg
 import * as styles from './Sponsors.module.scss';
 
 export default function Sponsors() {
-  // const { sponsors, site } = useStaticQuery(query);
-  const sponsorsEnabled = false; // site.siteMetadata.featureFlags.sponsors;
+  const { sponsors, site } = useStaticQuery(query);
+  const sponsorsEnabled = site.siteMetadata.featureFlags.sponsors;
 
   return (
     <Section
@@ -50,11 +50,13 @@ export default function Sponsors() {
       {sponsorsEnabled && (
         <Card className={styles.sponsors} backgroundColor='white' type='flat'>
           {sponsors.nodes
-            .filter(tier => (tier.data.Sponsors ?? []).length)
+            .filter((tier) => tier?.data?.Sponsors)
             .map((tier) => (
               <div key={tier.recordId} style={{ '--scale': tier.data.Scale }}>
                 <Text transform='uppercase' type='meta1' as='h3'>
-                  {tier.data.Name} Sponsors
+                  {tier.data.Name === 'Support'
+                    ? 'With Support From'
+                    : tier.data.Name + ' Sponsors'}
                 </Text>
                 <ul className={styles.sponsors__list}>
                   {tier.data.Sponsors.map((sponsor) => (
@@ -69,7 +71,10 @@ export default function Sponsors() {
                           image={getImage(sponsor.data.Logo.localFiles[0])}
                           style={
                             sponsor.data.Scale !== null
-                              ? { '--scale': tier.data.Scale * sponsor.data.Scale }
+                              ? {
+                                  '--scale':
+                                    tier.data.Scale * sponsor.data.Scale,
+                                }
                               : undefined
                           }
                           alt={`${sponsor.data.Name}'s logo`}
@@ -82,8 +87,7 @@ export default function Sponsors() {
                   ))}
                 </ul>
               </div>
-            ))
-          }
+            ))}
         </Card>
       )}
     </Section>
