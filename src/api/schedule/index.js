4b96ea686d3bbc91f2610647893d25126bff4fd5
Nish7 <nishilkapadia17@gmail.com>
2022-10-07 18:41:19 -0400

    schedule htv7

--- src/api/schedule/index.js
+++ src/api/schedule/index.js
@@ -1,28 +1,31 @@
 import Airtable from 'airtable';
 import Color from 'color';
 
+
+
+
+
 const base = new Airtable({
-  apiKey: process.env.AIRTABLE_API_KEY
-}).base(process.env.EVENTS_AIRTABLE_BASE);
+  apiKey: process.env.AIRTABLE_API_KEY,
+}).base(process.env.AIRTABLE_SCHEDULE_BASE_ID);
 
 const checkSameDay = (d1, d2) => {
   return d1.split('T')[0] === d2.split('T')[0];
-}
+};
 
 export default async function handler(req, res) {
-  const [ _events, _metadata ] = await Promise.all([
-    base('Events').select({
-      filterByFormula: '{Active} = TRUE()',
-      sort: [
-        { field: 'Start' },
-        { field: '_Duration' },
-      ],
-    }).all(),
+  const [_events, _metadata] = await Promise.all([
+    base('Events')
+      .select({
+        filterByFormula: '{Active} = TRUE()',
+        sort: [{ field: 'Start' }, { field: '_Duration' }],
+      })
+      .all(),
     base('Metadata').select().all(),
   ]);
 
   const metadata = _metadata.reduce((acc, meta) => {
-    switch(meta.fields['Data Type']) {
+    switch (meta.fields['Data Type']) {
       case 'Boolean':
         acc[meta.fields.Key] = meta.fields.Value === 'true';
         break;
@@ -39,40 +42,47 @@ export default async function handler(req, res) {
         acc[meta.fields.Key] = meta.fields.Value;
         break;
       default:
-        console.error(`Unknown Type ${meta.fields['Data Type']} for ${meta.fields.Key}`);
+        console.error(
+          `Unknown Type ${meta.fields['Data Type']} for ${meta.fields.Key}`,
+        );
         break;
     }
     return acc;
   }, {});
   metadata.DAY_SPAN = 1440 / metadata.MINUTES_PER_CELL;
 
-  const getDateTimeSpan = date => {
-    const [ hours, minutes ] = date.split('T')[1].split(':');
-    return ((parseInt(hours) * 60) + parseInt(minutes)) / metadata.MINUTES_PER_CELL;
+  const getDateTimeSpan = (date) => {
+    const [hours, minutes] = date.split('T')[1].split(':');
+    return (
+      (parseInt(hours) * 60 + parseInt(minutes)) / metadata.MINUTES_PER_CELL
+    );
   };
   const events = _events.reduce((acc, event) => {
     const isSameDay = checkSameDay(event.fields['Start'], event.fields['End']);
     acc[event.fields['_Day']] = acc[event.fields['_Day']] ?? [];
     acc[event.fields['_Day']].push({
       title: event.fields['Title'],
-      description: event.fields['Description'],
-      hopin: event.fields['Hopin Link'],
+      description: event.fields?.['Description'],
+      hopin: event.fields?.['Hopin Link'],
+      location: event.fields?.['Location'],
       start: event.fields['Start'],
       end: event.fields['End'],
       id: event.id,
       meta: {
         span: event.fields['_Duration'] / metadata.MINUTES_PER_CELL,
         start: getDateTimeSpan(event.fields['Start']),
-        end: getDateTimeSpan(event.fields['End']) + (isSameDay ? 0 : metadata.DAY_SPAN),
+        end:
+          getDateTimeSpan(event.fields['End']) +
+          (isSameDay ? 0 : metadata.DAY_SPAN),
         duration: event.fields['_Duration'],
       },
       type: {
-        name: event.fields['Name (From Type)'],
-        color: Color(event.fields['Color (from Type)'][0]).object()
+        name: event.fields?.['Name (From Type)'],
+        color: Color(event.fields['Color (from Type)']?.[0]).object(),
       },
       hosts: (event.fields['Name (from Hosts)'] ?? []).map((name, idx) => ({
-        position: event.fields['Position (from Hosts)'][idx],
-        id: event.fields['Hosts'][idx],
+        position: event.fields?.['Position (from Hosts)']?.[idx],
+        id: event.fields?.['Hosts']?.[idx],
         name,
       })),
     });
--- src/sections/Schedule/Events/index.jsx
+++ src/sections/Schedule/Events/index.jsx
@@ -1,38 +1,48 @@
 import Card from "@htv/ui-kit/components/Card";
-import Text from "@htv/ui-kit/components/Text";
-import { list, time, event, eventCard, eventBar, eventContent, eventTime, eventDetailsHead, eventDetailsMeta } from './Events.module.scss';
+import Text from '@htv/ui-kit/components/Text';
 
-const ORIGINS = {
-  'https://app.hopin.com': 'Hopin',
-  'https://teams.microsoft.com': 'Microsoft Teams',
-};
-
+import {
+  list,
+  time,
+  event,
+  eventCard,
+  eventBar,
+  eventContent,
+  eventTime,
+  eventDetailsHead,
+  eventDetailsMeta,
+} from './Events.module.scss';
 export default function Events({ items, metadata }) {
   if (!items?.length) return null;
 
   const last = items[items.length - 1];
   const first = items[0];
 
-  const cellToTime = cell => {
+  const cellToTime = (cell) => {
     const rawMinutes = cell * metadata.MINUTES_PER_CELL;
     const minutes = rawMinutes % 60;
     const hours = (rawMinutes - minutes) / 60;
 
-    return `${(hours % 12) || 12}:${minutes.toString().padStart(2, '0')}${(hours < 12 || hours >= 24) ? 'am' : 'pm'}`;
+    return `${hours % 12 || 12}:${minutes.toString().padStart(2, '0')}${
+      hours < 12 || hours >= 24 ? 'am' : 'pm'
+    }`;
   };
 
   // Who needs leetcode when you have to build a new schedule every hackathon every year?
-  const [ maxElevation ] = items
+  const [maxElevation] = items
     .reduce((acc, item) => {
       acc[item.meta.start] = (acc[item.meta.start] ?? 0) + 1;
       acc[item.meta.end] = (acc[item.meta.end] ?? 0) - 1;
       return acc;
     }, [])
-    .reduce((acc, item) => {
-      acc[1] += item;
-      if (acc[1] > acc[0]) acc[0] = acc[1];
-      return acc;
-    }, [0, 0]);
+    .reduce(
+      (acc, item) => {
+        acc[1] += item;
+        if (acc[1] > acc[0]) acc[0] = acc[1];
+        return acc;
+      },
+      [0, 0],
+    );
 
   const busyTil = new Array(maxElevation).fill(0);
   const renderItems = items.map((item, _, items) => {
@@ -40,11 +50,18 @@ export default function Events({ items, metadata }) {
     let colspan = 1;
 
     // If no conflicts at all
-    if (items.some(_item => _item !== item && item.meta.start < _item.meta.end && _item.meta.start < item.meta.end)) {
-      colstart = busyTil.findIndex(b => b <= item.meta.start);
+    if (
+      items.some(
+        (_item) =>
+          _item !== item &&
+          item.meta.start < _item.meta.end &&
+          _item.meta.start < item.meta.end,
+      )
+    ) {
+      colstart = busyTil.findIndex((b) => b <= item.meta.start);
       busyTil[colstart++] = item.meta.end;
     } else {
-      busyTil.forEach((_, idx) => busyTil[idx] = item.meta.end);
+      busyTil.forEach((_, idx) => (busyTil[idx] = item.meta.end));
       colspan = maxElevation;
     }
 
@@ -77,12 +94,12 @@ export default function Events({ items, metadata }) {
           </Text>
         </li>
       ))}
-      {renderItems.map(item => {
-        const { origin } = new URL(item?.hopin || 'https://hackthevalley.io');
+      {renderItems.map((item) => {
+        // const { origin } = new URL(item?.hopin || 'https://hackthevalley.io');
         return (
           <li
             style={{
-              '--color': `${item.type.color.r},${item.type.color.g},${item.type.color.b}`,
+              '--color': `${item.type.color.r},${item?.type?.color.g},${item?.type?.color.b}`,
               '--row': item.meta.start - first.meta.start + 1,
               '--row-span': item.meta.span,
               '--col': item.meta.colstart + 1,
@@ -92,7 +109,7 @@ export default function Events({ items, metadata }) {
             key={item.id}
           >
             <Card className={eventCard}>
-              <div className={eventBar}/>
+              <div className={eventBar} />
               <div className={eventContent}>
                 <div className={eventDetailsHead}>
                   <Text type='meta1' className={eventTime}>
@@ -102,9 +119,15 @@ export default function Events({ items, metadata }) {
                     {item.title}
                   </Text>
                 </div>
-                <Text type='meta1' color='white' lineHeight='relaxed' className={eventDetailsMeta}>
+                <Text
+                  type='meta1'
+                  color='white'
+                  lineHeight='relaxed'
+                  className={eventDetailsMeta}
+                >
                   <span>
-                    Location: <Text
+                    Location:{' '}
+                    <Text
                       type='meta1'
                       color='lime'
                       as='a'
@@ -112,12 +135,15 @@ export default function Events({ items, metadata }) {
                       target='_blank'
                       rel='noopener noreferrer'
                     >
-                      {ORIGINS[origin] ?? 'Somewhere uwu'}
+                      {item.location ?? 'Somewhere uwu'}
                     </Text>
                   </span>
                   <span>|</span>
                   <span>
-                    Host: {item.hosts.map(host => `${host.name} @ ${host.position}`).join(', ') || 'N/A'}
+                    Host:{' '}
+                    {item.hosts
+                      .map((host) => `${host.name} @ ${host.position}`)
+                      .join(', ') || 'N/A'}
                   </span>
                 </Text>
               </div>
