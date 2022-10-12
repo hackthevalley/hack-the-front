8d6a5553b8645cfa6cb805255e12e241bcc23400
Nishil Kapadia <nishilkapadia17@gmail.com>
2022-10-11 20:02:40 -0400

    bug event-card (#86)

--- src/sections/Schedule/Events/index.jsx
+++ src/sections/Schedule/Events/index.jsx
@@ -6,17 +6,6 @@ import Text from '@htv/ui-kit/components/Text';
 import { list, time, event, eventCard, eventBar, eventContent, eventTime, eventDetailsHead, eventDetailsMeta } from './Events.module.scss';
 
 
-import {
-  list,
-  time,
-  event,
-  eventCard,
-  eventBar,
-  eventContent,
-  eventTime,
-  eventDetailsHead,
-  eventDetailsMeta,
-} from './Events.module.scss';
 export default function Events({ items, metadata }) {
   if (!items?.length) return null;
 
@@ -147,7 +136,9 @@ export default function Events({ items, metadata }) {
                   <span>
                     Host:{' '}
                     {item.hosts
-                      .map(({name, position}) => position ? `${name} @ ${position}` : name )
+                      .map(({ name, position }) =>
+                        position ? `${name} @ ${position}` : name,
+                      )
                       .join(', ') || 'N/A'}
                   </span>
                 </Text>
