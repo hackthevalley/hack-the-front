3ef95214d170ab4a278e42bae74361ab436719a4
Shreyas Rao <raoshreyas2004@gmail.com>
2024-05-17 15:29:20 -0400

    resized carousel and updated nav bar

--- src/sections/Home/EventPhotos/index.jsx
+++ src/sections/Home/EventPhotos/index.jsx
@@ -1,18 +1,22 @@
+import classNames from 'classnames';
 import React from 'react';
+
 import { graphql, StaticQuery } from 'gatsby';
 import Img from "gatsby-image";
 import { Container, Carousel } from 'react-bootstrap';
 import Text from '@htv/ui-kit/components/Text';
+
 import * as styles from './EventPhotos.module.scss';
-import Layout from '../../../components/Layout';
+import Section from '@htv/ui-kit/components/Section';
 
 const EventPhotos = ({ data }) => (
     <div className={styles.carousel}>
         <Container className={styles.container}>
             <Carousel className={styles.transparentCarousel}>
                 {data.eventPhotos.edges.map(image => (
-                    <Carousel.Item key={image.node.id}>
-                        <Img 
+                    <Carousel.Item key={image.node.id} className={styles.imageContainer}>
+                        <Img
+                            className={styles.image}
                             fluid={image.node.childImageSharp.fluid}
                             alt={image.node.base.split(".")[0]}
                         />
@@ -25,22 +29,27 @@ const EventPhotos = ({ data }) => (
 
 export default function EventPhotosWithQuery() {
     return (
-        <div className={styles.carousel} backgroundColor='charcoal'>
-            <div className={styles.carousel_header}>
-                <Text
-                className={styles.carousel_heading}
-                type='heading2'
-                transform='uppercase'
-                weight='normal'
-                >
-                    A look into <Text type='heading2' color='lime' as='span'>Last Year</Text>...
-                </Text>
+        <Section
+            backgroundColor='charcoal'
+            className={styles.container}
+            id='pastImages'>
+            <div className={styles.carousel} backgroundColor='charcoal'>
+                <div className={styles.carousel_header}>
+                    <Text
+                    className={styles.carousel_heading}
+                    type='heading2'
+                    transform='uppercase'
+                    weight='normal'
+                    >
+                        A look into <Text type='heading2' color='lime' as='span'>Last Year</Text>...
+                    </Text>
+                </div>
+                <StaticQuery
+                    query={imageQuery}
+                    render={data => <EventPhotos data={data} />}
+                />
             </div>
-            <StaticQuery
-                query={imageQuery}
-                render={data => <EventPhotos data={data} />}
-            />
-        </div>
+        </Section>
     );
 }
 
