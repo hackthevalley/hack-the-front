44e009cadfe35106e35efe188e6f70a819a5252c
conrad-mo <51024076+conrad-mo@users.noreply.github.com>
2025-03-25 18:59:36 -0400

    Fix fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,52 +12,52 @@ const euclidCircularB = localFont({
   variable: "--font-euclid-circular-b",
   src: [
     {
-      path: "/public/fonts/Euclid-Circular-B-Bold.ttf",
+      path: "./assets/fonts/Euclid-Circular-B-Bold.ttf",
       weight: "700",
       style: "normal",
     },
     {
-      path: "/public/fonts/Euclid-Circular-B-Bold-Italic.ttf",
+      path: "./assets/fonts/Euclid-Circular-B-Bold-Italic.ttf",
       weight: "700",
       style: "italic",
     },
     {
-      path: "/public/fonts/Euclid-Circular-B-Italic.ttf",
+      path: "./assets/fonts/Euclid-Circular-B-Italic.ttf",
       weight: "400",
       style: "italic",
     },
     {
-      path: "/public/fonts/Euclid-Circular-B-Light.ttf",
+      path: "./assets/fonts/Euclid-Circular-B-Light.ttf",
       weight: "300",
       style: "normal",
     },
     {
-      path: "/public/fonts/Euclid-Circular-B-Light-Italic.ttf",
+      path: "./assets/fonts/Euclid-Circular-B-Light-Italic.ttf",
       weight: "300",
       style: "italic",
     },
     {
-      path: "/public/fonts/Euclid-Circular-B-Medium.ttf",
+      path: "./assets/fonts/Euclid-Circular-B-Medium.ttf",
       weight: "500",
       style: "normal",
     },
     {
-      path: "/public/fonts/Euclid-Circular-B-Medium-Italic.ttf",
+      path: "./assets/fonts/Euclid-Circular-B-Medium-Italic.ttf",
       weight: "500",
       style: "italic",
     },
     {
-      path: "/public/fonts/Euclid-Circular-B-Regular.ttf",
+      path: "./assets/fonts/Euclid-Circular-B-Regular.ttf",
       weight: "400",
       style: "normal",
     },
     {
-      path: "/public/fonts/Euclid-Circular-B-SemiBold.ttf",
+      path: "./assets/fonts/Euclid-Circular-B-SemiBold.ttf",
       weight: "600",
       style: "normal",
     },
     {
-      path: "/public/fonts/Euclid-Circular-B-SemiBold-Italic.ttf",
+      path: "./assets/fonts/Euclid-Circular-B-SemiBold-Italic.ttf",
       weight: "600",
       style: "italic",
     },
