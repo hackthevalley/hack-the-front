7329c85b852e26a5122ca858e4edd5e29f59e78c
Preston Tom-Ying <preston.tomying@gmail.com>
2025-03-28 16:30:59 -0400

    add major components on landing page

--- app/page.tsx
+++ app/page.tsx
@@ -2,101 +2,61 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-euclid-circular-b)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    // Started div & main tags provided by Next.js, we can modify it
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen bg-[#06101E] p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-euclid-circular-b)]">
+      
+      {/* Dotted flower art */}
+      <img className="absolute bottom-[-9rem] left-[-7rem]" src="/pointillism/dot-grey-flower.svg" alt="pointilism flower" width="448"/>
+      <img className="absolute top-[19rem] right-[-8rem]" src="/pointillism/dot-green-flower.svg" alt="pointilism flower" width="448"/>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        
+
+        <div className="grid grid-cols-[55%_45%] gap-4">
+        <div className="w-100 grid grid-cols-[10%_90%] gap-2">
+          {/* Positioning the ">" */}
+          <div className="flex justify-center mt-[2.3rem]">
+            <h1 className="text-[#FFFFFF] text-7xl font-medium">{">"}</h1>
+          </div>
+          <div className="flex flex-col gap-y-4">
+            <p className="text-[#9B9EA1] text-xl font-[family-name:var(--font-source-code-pro)]">Up for the challenge?</p>
+            <h1 className="text-[#FFFFFF] text-7xl font-medium">HACK THE VALLEY X</h1>
+
+            {/* Date */}
+            <div className="flex-col">
+
+            <div className="flex items-center justify-center gap-x-4 mb-[4rem]">
+              <hr className="text-[#A5D6A7] w-[20%]" />
+              <p className="text-[#A5D6A7] text-lg font-[family-name:var(--font-source-code-pro)]">Oct. 3-5, 2025</p>
+              <hr className="text-[#A5D6A7] w-[20%]" />
+            </div>
+
+            {/* Apply Button */}
+            <div className="flex justify-center">
+              <button className="rounded-xl p-4 text-[#F5F3E7] w-[16rem] bg-linear-to-b from-[#6EAC5E] to-[#6B8F9F]">
+                  <p className="font-bold text-lg">Apply Now</p>
+              </button>
+            </div>
+
+            </div>
+          </div>
         </div>
+
+        <div className="relative w-[700px]">
+          <img className="" src="/landing-page/date-card-2.svg" alt="Event Card" width="573"/>
+          <img className="absolute top-[-11rem] left-[-3rem]" src="/landing-page/event-card-2.svg" alt="Event Card" width="662"/>
+          <img className="absolute bottom-[22rem] left-[-2.5rem]" src="/metallics/clover.svg" alt="Metallic Clover" width="151"/>
+          <img className="absolute top-[12.5rem] left-[24rem]" src="/metallics/flower.svg" alt="Metallic Flower" width="135"/>
+        </div>
+
+        </div>
+
+
+
+
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+       
       </footer>
     </div>
   );
