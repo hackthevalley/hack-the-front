13fd5281aaff0610be73c8fda1bd993ec5910c81
Preston Tom-Ying <preston.tomying@gmail.com>
2025-05-05 15:31:12 -0400

    starting login page

---
+++ app/login/layout.tsx
@@ -0,0 +1,86 @@
+import type { Metadata } from "next";
+import { Source_Code_Pro } from "next/font/google";
+import localFont from "next/font/local";
+import "../globals.css";
+
+const sourceCodePro = Source_Code_Pro({
+  variable: "--font-source-code-pro",
+  subsets: ["latin"],
+});
+
+const euclidCircularB = localFont({
+  variable: "--font-euclid-circular-b",
+  src: [
+    {
+      path: "../assets/fonts/Euclid-Circular-B-Bold.ttf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../assets/fonts/Euclid-Circular-B-Bold-Italic.ttf",
+      weight: "700",
+      style: "italic",
+    },
+    {
+      path: "../assets/fonts/Euclid-Circular-B-Italic.ttf",
+      weight: "400",
+      style: "italic",
+    },
+    {
+      path: "../assets/fonts/Euclid-Circular-B-Light.ttf",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../assets/fonts/Euclid-Circular-B-Light-Italic.ttf",
+      weight: "300",
+      style: "italic",
+    },
+    {
+      path: "../assets/fonts/Euclid-Circular-B-Medium.ttf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../assets/fonts/Euclid-Circular-B-Medium-Italic.ttf",
+      weight: "500",
+      style: "italic",
+    },
+    {
+      path: "../assets/fonts/Euclid-Circular-B-Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../assets/fonts/Euclid-Circular-B-SemiBold.ttf",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "../assets/fonts/Euclid-Circular-B-SemiBold-Italic.ttf",
+      weight: "600",
+      style: "italic",
+    },
+  ],
+});
+
+export const metadata: Metadata = {
+  title: "Hack the Valley",
+  description:
+    "Join 750 innovative and creative developers, designers, and creators for 36 hours of hacking. You'll get access to some of the best hardware and APIs on the market. Plus get to meet some experienced and awesome mentors! All this in just one weekend? I know, it's hard to believe.",
+};
+
+export default function LoginLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <section className={`${sourceCodePro.variable}
+    ${euclidCircularB.variable}
+    antialiased relative`}>
+      {children}
+
+    </section>
+  );
+}
---
+++ app/login/page.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+export default function LoginPage() {
+    return(
+        <div className="bg-black ">
+            {/* Back button */}
+
+            <div>
+                <p className="text-black">$ npm start challenge</p>
+
+                <p className="text-black text-xl">{">"} Welcome Back Hacker,</p>
+                
+                <div className="w-full">
+                    <div className="flex justify-between align-items">
+                        <hr />
+                        <p>Sign in to view dashboard</p>
+                        <hr />
+                    </div>
+
+                    {/* Custom field component for Email */}
+
+                    {/* Custom field component for Password */}
+                    
+
+                </div>
+
+            </div>
+        </div>
+    )
+}
