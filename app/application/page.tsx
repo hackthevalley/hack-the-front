4260c71f1a04ca68095f095dbefcff385002d6dd
Preston Tom-Ying <preston.tomying@gmail.com>
2025-06-01 18:50:09 -0400

    fixed scrollbar UI

--- app/application/page.tsx
+++ app/application/page.tsx
@@ -11,45 +11,45 @@ import { FormSections } from "@/application/FormSections";
 import { JSX, useState } from "react";
 import { form } from "motion/react-client";
 
-
 // TODO: Need to update to pull questions from backend DB
-const formSections: { key: string; position: string, content: JSX.Element }[] = [
-  {
-    key: "section1",
-    position: "justify-start",
-    content: <FormSections.ProfileInfo />,
-  },
-  {
-    key: "section2",
-    position: "justify-end",
-    content: <FormSections.SchoolInfo />,
-  },
-  {
-    key: "section3",
-    position: "justify-start",
-    content: <FormSections.DemographyInfo />,
-  },
-  {
-    key: "section4",
-    position: "justify-end",
-    content: <FormSections.ExperienceInfo />,
-  },
-  {
-    key: "section5",
-    position: "justify-start",
-    content: <FormSections.SkillConfidence />,
-  },
-  {
-    key: "section7",
-    position: "justify-center",
-    content: <FormSections.MLH />,
-  },
-  {
-    key: "section8",
-    position: "justify-center",
-    content: <FormSections.ConsentInfo />,
-  },
-];
+const formSections: { key: string; position: string; content: JSX.Element }[] =
+  [
+    {
+      key: "section1",
+      position: "justify-start",
+      content: <FormSections.ProfileInfo />,
+    },
+    {
+      key: "section2",
+      position: "justify-end",
+      content: <FormSections.SchoolInfo />,
+    },
+    {
+      key: "section3",
+      position: "justify-start",
+      content: <FormSections.DemographyInfo />,
+    },
+    {
+      key: "section4",
+      position: "justify-end",
+      content: <FormSections.ExperienceInfo />,
+    },
+    {
+      key: "section5",
+      position: "justify-start",
+      content: <FormSections.SkillConfidence />,
+    },
+    {
+      key: "section7",
+      position: "justify-center",
+      content: <FormSections.MLH />,
+    },
+    {
+      key: "section8",
+      position: "justify-center",
+      content: <FormSections.ConsentInfo />,
+    },
+  ];
 export default function Application() {
   const [isFormActive, setIsFormActive] = useState(false);
   const appRef = useRef<HTMLDivElement>(null);
@@ -62,7 +62,7 @@ export default function Application() {
   return (
     <>
       <Navbar hide={true} />
-      <div className="h-screen overflow-y-scroll snap-y snap-mandatory bg-black text-white">
+      <div className="h-[calc(100vh-10rem)] overflow-y-scroll no-scrollbar snap-y snap-mandatory bg-black text-white">
         <section className="h-screen snap-start flex flex-col items-center justify-center relative px-4 md:px-10">
           <Card className="w-8/12 text-center relative">
             <div className="mb-10">
@@ -72,10 +72,15 @@ export default function Application() {
             </div>
             <p className="text-xl mb-10">
               Hey hacker, welcome aboard&#x1F44B;&#x1F3FC; <br />
-              Your co-pilot <strong>(name)</strong> will track your journey through this application. Buckle up!
+              Your co-pilot <strong>(name)</strong> will track your journey
+              through this application. Buckle up!
             </p>
             <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.95 }}>
-              <Button text="Begin now" extraClass="py-3 cursor-pointer" onClick={handleStart} />
+              <Button
+                text="Begin now"
+                extraClass="py-3 cursor-pointer"
+                onClick={handleStart}
+              />
             </motion.div>
             <Image
               src="/application-page/spaceship.png"
@@ -91,11 +96,22 @@ export default function Application() {
           <section
             key={section.key}
             ref={index === 0 ? appRef : undefined}
-            className={`h-screen snap-start items-center flex px-4 md:px-10 mx-20 ${section.position} ${index === formSections.length - 1 ? "flex-col" : ""} gap-10`}>
+            className={`h-screen snap-start items-center flex px-4 md:px-10 mx-20 ${
+              section.position
+            } ${index === formSections.length - 1 ? "flex-col" : ""} gap-10`}
+          >
             {section.content}
             {index === formSections.length - 1 && (
-              <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.95 }} className="mt-10">
-                <Button text="Submit Application" extraClass="py-3 cursor-pointer" onClick={() => alert("Form submitted!")} />
+              <motion.div
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.95 }}
+                className="mt-10"
+              >
+                <Button
+                  text="Submit Application"
+                  extraClass="py-3 cursor-pointer"
+                  onClick={() => alert("Form submitted!")}
+                />
               </motion.div>
             )}
           </section>
