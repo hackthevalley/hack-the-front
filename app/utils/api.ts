6084031a537285b257f27e10c6c96c8d8af1914f
Preston Tom-Ying <83887988+prestonty@users.noreply.github.com>
2025-06-03 20:08:03 -0400

    Form Validation in Login and Sign Up (#140)
    * add major components on landing page
    
    * responsiveness for big screens
    
    * created button component
    
    * navbar UI
    
    * attempted to make colours variables
    
    * made all colours variables and fixed styling
    
    * fixed navbar
    
    * event card animations
    
    * landing page animations
    
    * finished landing page
    
    * moved navbar out of layout.tsx
    
    * Signup page (#137)
    
    * starting login page
    
    * modified navbar for login
    
    * started login page
    
    * fixed overflow
    
    * modified button components for routes
    
    * modified button components for routes 2
    
    * added basic UI elements on login page
    
    * create textfield components
    
    * create textfield components (#135)
    
    * main UI of login done
    
    * updates
    
    * Delete tailwind.config.mjs
    
    * updates
    
    * Update page.tsx
    
    ---------
    
    Co-authored-by: Preston Tom-Ying <preston.tomying@gmail.com>
    
    * fixed navbar
    
    * fixed bugs
    
    * revised changes
    
    * landing-page with new design
    
    * adjusted htv title
    
    * switched title-image
    
    * Swap from png to text
    
    * feat: login page - form fields change button
    
    * feat: password fields can be toggled
    
    * add form validation
    
    * add sign up and login api calls
    
    ---------
    
    Co-authored-by: Kate Huang <123223306+Lilsofie@users.noreply.github.com>
    Co-authored-by: conrad-mo <51024076+conrad-mo@users.noreply.github.com>

--- app/utils/api.ts
+++ app/utils/api.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = process.env.API_BASE_URL;
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 interface FetchOptions {
   method?: "GET" | "POST" | "PUT" | "DELETE" | "PATCH";
