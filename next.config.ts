0a137b2063532414fbce90b8f7bc26372ec1d9d5
conrad-mo <51024076+conrad-mo@users.noreply.github.com>
2025-07-13 22:04:49 -0400

    Remove experimental nextjs flags

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    formats: ["image/avif", "image/webp"],
-    dangerouslyAllowSVG: true,
-    contentDispositionType: "attachment",
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-  },
+  /* config options here */
 };
 
 export default nextConfig;
